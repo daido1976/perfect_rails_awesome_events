d28fe458e790218d3d94733d6f9ab8e55457a828
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-05-22 16:08:10 +0900

    sessions#createのrequest specのテストを作成

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -4,19 +4,50 @@ RSpec.describe SessionsController, type: :request do
   describe '#create' do
     before do
       OmniAuth.config.test_mode = true
-      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
         provider: 'twitter',
         uid: '12354567890',
         info: {
           nickname: 'hogehoge',
-          image: 'http://image.example.com'
+          image: 'http://image.example.com',
         },
-      })
+      )
     end
 
-    it 'ユーザを新規作成すること' do
-      get '/auth/twitter'
-      expect { get '/auth/twitter/callback' }.to change { User.all.count }.from(0).to(1)
+    context 'ユーザがDBに存在していない場合' do
+      it 'ユーザを新規作成すること' do
+        get '/auth/twitter'
+        expect { get '/auth/twitter/callback' }.to change { User.all.count }.by(1)
+      end
+
+      it 'sessionにIDが保存されていること' do
+        get '/auth/twitter/callback'
+        expect(session[:user_id]).to be_present
+      end
+
+      it 'ログイン後トップページにリダイレクトすること' do
+        get '/auth/twitter/callback'
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'ユーザがDBにすでに存在している場合' do
+      before { get '/auth/twitter/callback' }
+
+      it '新しいユーザが作成されないこと' do
+        get '/auth/twitter'
+        expect { get '/auth/twitter/callback' }.not_to change { User.all.count }
+      end
+
+      it 'sessionにIDが保存されていること' do
+        get '/auth/twitter/callback'
+        expect(session[:user_id]).to be_present
+      end
+      
+      it 'ログイン後トップページにリダイレクトすること' do
+        get '/auth/twitter/callback'
+        expect(response).to redirect_to root_path
+      end
     end
   end
 end
