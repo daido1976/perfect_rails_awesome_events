7b92dc52ca4c6fdd7f18a66a7f67ea3b98661637
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-05-22 13:19:41 +0900

    twitterのOAuth認証を使ったユーザ新規作成のテスト

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :request do
+  describe '#create' do
+    before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+        provider: 'twitter',
+        uid: '12354567890',
+        info: {
+          nickname: 'hogehoge',
+          image: 'http://image.example.com'
+        },
+      })
+    end
+
+    it 'ユーザを新規作成すること' do
+      get '/auth/twitter'
+      expect { get '/auth/twitter/callback' }.to change { User.all.count }.from(0).to(1)
+    end
+  end
 end
