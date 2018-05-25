797b0fbf042d91c60e83954f92ffc6e3f6b4fca7
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-05-25 14:49:40 +0900

    $ rails g resource event owner_id:integer name:string place:string start_time:datetime end_time:datetime content:text

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :event do
+    owner_id 1
+    name 'MyString'
+    place 'MyString'
+    start_time '2018-05-25 14:47:58'
+    end_time '2018-05-25 14:47:58'
+    content 'MyText'
+  end
+end
