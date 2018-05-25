a0cee9e4aa365830029925e15d216caf980cabbd
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-05-25 15:28:09 +0900

    Event モデルの各属性にバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
 class Event < ApplicationRecord
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :place, length: { maximum: 100 }, presence: true
+  validates :content, length: { maximum: 2000 }, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :end_time_should_be_after_start_time
+
+  private
+
+  def end_time_should_be_after_start_time
+    return unless start_time && end_time
+
+    errors.add(:end_time, 'は開始時間よりも後に設定してください') if end_time <= start_time
+  end
 end
