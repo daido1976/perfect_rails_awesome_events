a65d454f74e53ff2ef86b95a7e29f7d58943ae17
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-05-31 16:14:34 +0900

    factory :user の sequence の数字の前にアンダースコアを入れる

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user, aliases: [:owner] do
     provider 'twitter'
-    sequence(:uid) { |i| "uid#{i}" }
-    sequence(:nickname) { |i| "nickname#{i}" }
-    sequence(:image_url) { |i| "http://example.com/image#{i}.jpg" }
+    sequence(:uid) { |i| "uid_#{i}" }
+    sequence(:nickname) { |i| "nickname_#{i}" }
+    sequence(:image_url) { |i| "http://example.com/image_#{i}.jpg" }
   end
 end
