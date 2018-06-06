0e8a506a6533d81de313f11716d239179eb1cb87
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-06 17:07:41 +0900

    request spec に events_controller の edit/update/destroy アクションのテストを追加

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -102,4 +102,104 @@ RSpec.describe EventsController, type: :request do
       it_behaves_like '@event に該当するイベントが格納されていること'
     end
   end
+
+  describe 'GET #edit' do
+    subject { get "/events/#{event_id}/edit" }
+
+    let(:event_id) { Event.last.id }
+
+    context 'ログイン済みのユーザがアクセスした場合' do
+      before { get '/auth/twitter/callback' }
+
+      context 'アクセスユーザが当該イベントのオーナーだった場合' do
+        before do
+          create(:event, owner_id: User.last.id)
+        end
+
+        it 'イベント編集ページが表示されること' do
+          subject
+          expect(response).to render_template('edit')
+        end
+      end
+
+      context 'アクセスユーザが当該イベントのオーナーでなかった場合' do
+        before do
+          create(:event)
+        end
+
+        it 'error404 のページが表示されること' do
+          subject
+          expect(response).to render_template('error404')
+        end
+      end
+    end
+
+    context '未ログインのユーザがアクセスした場合' do
+      before do
+        create(:event)
+      end
+
+      it 'トップページへリダイレクトされること' do
+        subject
+        expect(response).to redirect_to root_path
+      end
+
+      it 'アラートが表示されること' do
+        subject
+        expect(flash[:alert]).to be_present
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    subject { patch "/events/#{event_id}", params: event_params }
+
+    before do
+      get '/auth/twitter/callback'
+      create(:event, owner_id: User.last.id, content: 'content')
+    end
+
+    let(:event_id) { Event.last.id }
+
+    context '正しい値が入力された場合' do
+      let(:event_params) { { event: attributes_for(:event, content: 'updated_content') } }
+
+      it 'イベント情報が更新されること' do
+        expect { subject }.to change { Event.find(event_id).content }.from('content').to('updated_content')
+      end
+
+      it 'events/:id にリダイレクトされること' do
+        subject
+        expect(response).to redirect_to("/events/#{event_id}")
+      end
+    end
+
+    context '正しい値が入力されなかった場合' do
+      let(:event_params) { { event: attributes_for(:event, content: 'updated_content').merge(name: '') } }
+
+      it 'イベント情報が更新されないこと' do
+        expect { subject }.not_to change { Event.find(event_id).content }
+      end
+
+      it 'イベント編集ページが再度表示されること' do
+        subject
+        expect(response).to render_template('edit')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    subject { delete "/events/#{event_id}" }
+
+    before do
+      get '/auth/twitter/callback'
+      create(:event, owner_id: User.last.id)
+    end
+
+    let(:event_id) { Event.last.id }
+
+    it 'イベントを削除すること' do
+      expect { subject }.to change { Event.count }.by(-1)
+    end
+  end
 end
