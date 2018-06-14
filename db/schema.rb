57774f21eb473854b1d5e4874943b33512cf57a6
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-14 12:12:44 +0900

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_525_054_758) do
+ActiveRecord::Schema.define(version: 2018_06_14_014439) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'events', force: :cascade do |t|
-    t.integer 'owner_id'
-    t.string 'name', null: false
-    t.string 'place', null: false
-    t.datetime 'start_time', null: false
-    t.datetime 'end_time', null: false
-    t.text 'content', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['owner_id'], name: 'index_events_on_owner_id'
+  create_table "events", force: :cascade do |t|
+    t.integer "owner_id"
+    t.string "name", null: false
+    t.string "place", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_events_on_owner_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'provider', null: false
-    t.string 'uid', null: false
-    t.string 'nickname', null: false
-    t.string 'image_url', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[provider uid], name: 'index_users_on_provider_and_uid', unique: true
+  create_table "tickets", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "event_id", null: false
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id", "user_id"], name: "index_tickets_on_event_id_and_user_id", unique: true
+    t.index ["event_id"], name: "index_tickets_on_event_id"
+    t.index ["user_id", "event_id"], name: "index_tickets_on_user_id_and_event_id", unique: true
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "nickname", null: false
+    t.string "image_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
+  end
+
+  add_foreign_key "tickets", "events"
+  add_foreign_key "tickets", "users"
 end
