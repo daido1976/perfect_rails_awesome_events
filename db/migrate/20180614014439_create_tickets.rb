19ffa2d431101ca9215844a35aa00a034f0bf095
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-14 10:51:46 +0900

    $ rails g model ticket user:references event:references comment:string

---
+++ db/migrate/20180614014439_create_tickets.rb
@@ -0,0 +1,11 @@
+class CreateTickets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tickets do |t|
+      t.references :user, foreign_key: true
+      t.references :event, foreign_key: true
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
