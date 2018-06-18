9846a826c7dbb417d2e208e1d61287198ff14b5e
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-18 16:42:30 +0900

    イベント参加用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  resources :events
+  resources :events do
+    resources :tickets
+  end
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
