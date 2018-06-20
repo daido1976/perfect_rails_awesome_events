dddf5c6062e325eeb3b18dae356c63e04bfa93ce
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-20 15:04:50 +0900

    イベント参加用のルーティングを :create, :destroy のみに設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   resources :events do
-    resources :tickets
+    resources :tickets, only: [:create, :destroy]
   end
 
   get '/auth/:provider/callback' => 'sessions#create'
