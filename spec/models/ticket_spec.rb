646d37d8eb13406b0fabb74ffd28e27b6e9d5472
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-20 16:55:15 +0900

    Comment モデルのバリデーションテストを作成

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#comment' do
+    it { should allow_value(nil).for(:comment) }
+    # 31文字の空文字を入力
+    it { should allow_value(' ' * 31).for(:comment) }
+    it { should validate_length_of(:comment).is_at_most(30) }
+  end
 end
