34e52aaf02ce6ebf4f162c2cf0a233bff6c4582b
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-21 12:45:19 +0900

    users_controller に retire アクションと destroy アクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate
+
+  def retire
+  end
+
+  def destroy
+    if current_user.destroy
+      session[:user_id] = nil
+      redirect_to root_path, notice: '退会完了しました'
+    else
+      render :retire
+    end
+  end
 end
