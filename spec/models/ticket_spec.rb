733d088b494fb8ee6e8b9d4b984317366ee53c38
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-06-21 15:06:48 +0900

    Comment モデルのバリデーションテストの意図が伝わるようコメントを修正

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Ticket, type: :model do
   describe '#comment' do
     it { should allow_value(nil).for(:comment) }
-    # 31文字の空文字を入力
+    # allow_nil ではなく、 allow_blank オプションを使用しているため、31文字以上の空文字を許容することを確認する
     it { should allow_value(' ' * 31).for(:comment) }
     it { should validate_length_of(:comment).is_at_most(30) }
   end
